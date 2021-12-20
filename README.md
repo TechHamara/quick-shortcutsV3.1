6ebae6978a8c1df684ca46076128b1b6bde3939a
Joseph Lee <facebooklee52@gmail.com>
2021-12-21 00:21:33 +0800

    Added new functions

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## QuickShortcuts
 
-An App Inventor 2 extension created using Rush.
+An App Inventor 2 extension created using Rush. Coming soon.
+This extension requires minimum Android SDK version 24 (Nongat) to function.
--- src/com/memelabs/quickshortcuts/QuickShortcuts.java
+++ src/com/memelabs/quickshortcuts/QuickShortcuts.java
@@ -2,9 +2,11 @@ package com.memelabs.quickshortcuts;
 
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
+import android.content.res.AssetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Icon;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
@@ -27,41 +29,89 @@ public class QuickShortcuts extends AndroidNonvisibleComponent {
     context = activity;
   }
 
-  @SimpleFunction(description = "Creates a shortcut with the given name, icon and starting screen.")
-  public void CreateShortcut(String id ,String name, String description, String icon, String intent) {
+  @SimpleFunction(description = "Creates a shortcut with the given name, icon, starting screen and value.")
+  public void CreateShortcut(String id ,String name, String description, String icon, String screen, String startValue) {
     ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
+    AssetManager assetManager = context.getAssets();
     String packageName = context.getClass().getPackage().getName();
     Intent shortcutIntent;
+    ShortcutInfo shortcut;
     try {
-      shortcutIntent = new Intent(Intent.ACTION_VIEW, null, context, Class.forName(packageName + "." + intent));
+      shortcutIntent = new Intent(Intent.ACTION_VIEW, null, context, Class.forName(packageName + "." + screen)).putExtra("APP_INVENTOR_START", startValue);
     } catch (Exception e) {
       throw new YailRuntimeError(e.toString(), "Exception");
     }
-    ShortcutInfo shortcut = new ShortcutInfo.Builder(context, id)
-      .setShortLabel(name)
-      .setLongLabel(description)
-      .setIcon(Icon.createWithContentUri("//" + icon))
-      .setIntent(shortcutIntent)
-      .build();
-    
+
+    try {
+      shortcut = new ShortcutInfo.Builder(context, id)
+        .setShortLabel(name)
+        .setLongLabel(description)
+        .setIcon(Icon.createWithBitmap(BitmapFactory.decodeStream(assetManager.open(icon))))
+        .setIntent(shortcutIntent)
+        .build();
+    } catch (Exception e) {
+      throw new YailRuntimeError(e.toString(), "IOException");
+    }
+
     shortcutManager.pushDynamicShortcut(shortcut);
   }
 
-  @SimpleFunction(description = "Removes the shortcut with the given name.")
+  @SimpleFunction(description = "Creates and requests to pin a shortcut with the given name, icon, starting screen and value.")
+  public void CreatePinnedShortcut(String id ,String name, String description, String icon, String screen, String startValue) {
+    ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
+    AssetManager assetManager = context.getAssets();
+    String packageName = context.getClass().getPackage().getName();
+    Intent shortcutIntent;
+    ShortcutInfo shortcut;
+    try {
+      shortcutIntent = new Intent(Intent.ACTION_VIEW, null, context, Class.forName(packageName + "." + screen)).putExtra("APP_INVENTOR_START", startValue);
+    } catch (Exception e) {
+      throw new YailRuntimeError(e.toString(), "Exception");
+    }
+
+    try {
+      shortcut = new ShortcutInfo.Builder(context, id)
+        .setShortLabel(name)
+        .setLongLabel(description)
+        .setIcon(Icon.createWithBitmap(BitmapFactory.decodeStream(assetManager.open(icon))))
+        .setIntent(shortcutIntent)
+        .build();
+    } catch (Exception e) {
+      throw new YailRuntimeError(e.toString(), "IOException");
+    }
+
+    shortcutManager.requestPinShortcut(shortcut, null);
+  }
+
+  @SimpleFunction(description = "Removes the shortcut with the given id.")
   public void RemoveShortcut(String id) {
     ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
     shortcutManager.removeDynamicShortcuts(Arrays.asList(id));
   }
 
-  @SimpleFunction(description = "Checks whether the user's device supports shortcuts.")
-  public boolean SupportsShortcuts() {
+  @SimpleFunction(description = "Disable a shortcut with the given id.")
+  public void DisableShortcut(String id) {
     ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
-    return shortcutManager.isRequestPinShortcutSupported();
+    shortcutManager.disableShortcuts(Arrays.asList(id));
   }
-  
+
   @SimpleFunction(description = "Removes all shortcuts.")
   public void RemoveAllShortcuts() {
     ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
     shortcutManager.removeAllDynamicShortcuts();
   }
+
+  @SimpleFunction(description = "Checks if shortcuts are rate-limited.") 
+  public boolean IsRateLimited() {
+    ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
+    return shortcutManager.isRateLimitingActive();
+  }
+
+  @SimpleFunction(description = "Checks whether the user's launcher supports pinning shortcuts.")
+  public boolean SupportsPinnedShortcuts() {
+    ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
+    return shortcutManager.isRequestPinShortcutSupported();
+  }
+  
+  
 }
