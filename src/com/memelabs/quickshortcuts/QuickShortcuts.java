1abe39e41ed661c315c68571319d19ca427e0053
Joseph Lee <facebooklee52@gmail.com>
2022-04-16 01:37:30 +0800

    Re-factor CreateDynamicShortcut for older devices

--- src/com/memelabs/quickshortcuts/QuickShortcuts.java
+++ src/com/memelabs/quickshortcuts/QuickShortcuts.java
@@ -17,6 +17,7 @@ import com.google.appinventor.components.runtime.util.YailList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class QuickShortcuts extends AndroidNonvisibleComponent {
 
@@ -35,33 +36,53 @@ public class QuickShortcuts extends AndroidNonvisibleComponent {
   }
 
   @SimpleFunction(description = "Creates a shortcut with the given name, icon, starting screen and value.")
-  public void CreateDynamicShortcut(String id ,String shortName, String longName, String icon, String screen, String startValue) {
+  public void CreateDynamicShortcut(YailList shortcuts) {
     if (compatibility) {
       ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
       AssetManager assetManager = context.getAssets();
       String packageName = context.getClass().getPackage().getName();
       Intent shortcutIntent;
       ShortcutInfo shortcut;
-      try {
-        shortcutIntent = new Intent(Intent.ACTION_VIEW, null, context, Class.forName(packageName + "." + screen)).putExtra("APP_INVENTOR_START", startValue);
-      } catch (Exception e) {
-        throw new YailRuntimeError(e.toString(), "Exception");
-      }
+      List<ShortcutInfo> shortcutList = new ArrayList();
 
-      try {
-        shortcut = new ShortcutInfo.Builder(context, id)
-          .setShortLabel(shortName)
-          .setLongLabel(longName)
-          .setIcon(Icon.createWithBitmap(BitmapFactory.decodeStream(assetManager.open(icon))))
-          .setIntent(shortcutIntent)
-          .build();
-      } catch (Exception e) {
-        throw new YailRuntimeError(e.toString(), "IOException");
+      for (int i = 1; i <= shortcuts.size(); i++) {
+        try {
+          YailList shortcutDetail = (YailList) shortcuts.get(i);
+          String id = (String) shortcutDetail.get(1);
+          String shortname = (String) shortcutDetail.get(2);
+          String longname = (String) shortcutDetail.get(3);
+          String icon = (String) shortcutDetail.get(4);
+          String screen = (String) shortcutDetail.get(5);
+          String startValue = (String) shortcutDetail.get(6);
+
+          try {
+            shortcutIntent = new Intent(Intent.ACTION_VIEW, null, context, Class.forName(packageName + "." + screen)).putExtra("APP_INVENTOR_START", startValue);
+          } catch (Exception e) {
+            throw new YailRuntimeError(e.toString(), "Icon Exception");
+          }
+
+          shortcut = new ShortcutInfo.Builder(context, id)
+            .setShortLabel(shortname)
+            .setLongLabel(longname)
+            .setIcon(Icon.createWithBitmap(BitmapFactory.decodeStream(assetManager.open(icon))))
+            .setIntent(shortcutIntent)
+            .build();
+
+          shortcutList.add(shortcut);
+        } catch (Exception e) {
+          throw new YailRuntimeError(e.toString(), "Shortcut Exception");
+        }
       }
 
       // 0.2.2: Fixed crash when adding shortcut below android 12
-      shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
+      shortcutManager.setDynamicShortcuts(shortcutList);
     }
+  }
+
+  @SimpleFunction(description = "Create list with information about the dynamic shortcut.")
+  public YailList BuildDynamicShortcut(String id ,String shortName, String longName, String icon, String screen, String startValue) {
+    YailList shortcut = YailList.makeList(Arrays.asList(id, shortName, longName, icon, screen, startValue));
+    return shortcut;
   }
 
   @SimpleFunction(description = "Creates and requests to pin a shortcut with the given name, icon, starting screen and value.")
@@ -75,7 +96,7 @@ public class QuickShortcuts extends AndroidNonvisibleComponent {
       try {
         shortcutIntent = new Intent(Intent.ACTION_VIEW, null, context, Class.forName(packageName + "." + screen)).putExtra("APP_INVENTOR_START", startValue);
       } catch (Exception e) {
-        throw new YailRuntimeError(e.toString(), "Exception");
+        throw new YailRuntimeError(e.toString(), "Shortcut Exception");
       }
 
       try {
